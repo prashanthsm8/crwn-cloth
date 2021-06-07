3140265131848d61116630e761356b56eb8ee8d7
prashanthsm8 <37961439+prashanthsm8@users.noreply.github.com>
2021-06-08 00:06:44 +0530

    2nd commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../../component/directory/directory.component'
+const Homepage = () => {
+    return(
+        <div className='homepage'>
+           <Directory/>
+        </div>
+    );
+
+};
+
+export default Homepage;
